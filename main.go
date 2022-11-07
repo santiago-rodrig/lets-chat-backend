a47e11a64eef8bd8cd708a61bd3ecd3970161c31
Santiago Rodriguez <santo1996.29@gmail.com>
2022-11-06 20:39:02 -0400

    put read-write pump in separate function

--- main.go
+++ main.go
@@ -10,42 +10,61 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var webSocketConnection *websocket.Conn
+var webSocketConnections = make(map[string]*websocket.Conn)
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		var err error
-		webSocketConnection, err = upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	})
-	go func() {
-		for {
-			if webSocketConnection != nil {
-				_, rdr, err := webSocketConnection.NextReader()
+func readWritePump(username string) {
+	for {
+		webSocketConnection, ok := webSocketConnections[username]
+		if ok {
+			_, rdr, err := webSocketConnection.NextReader()
+			if err != nil {
+				log.Println(err)
+			}
+			if rdr != nil {
+				_, err := io.ReadAll(rdr)
 				if err != nil {
 					log.Println(err)
 				}
-				data, err := io.ReadAll(rdr)
-				if err != nil {
-					log.Println(err)
-				}
-				wtr, err := webSocketConnection.NextWriter(websocket.TextMessage)
-				if err != nil {
-					log.Println(err)
-				}
-				wtr.Write(data)
+			}
+			wtr, err := webSocketConnection.NextWriter(websocket.TextMessage)
+			if err != nil {
+				log.Println(err)
+			}
+			if wtr != nil {
 				err = wtr.Close()
 				if err != nil {
 					log.Println(err)
 				}
 			}
 			time.Sleep(250 * time.Millisecond)
+		} else {
+			return
 		}
-	}()
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		cookies := r.Cookies()
+		var username string
+		for _, cookie := range cookies {
+			if cookie.Name == "username" {
+				username = cookie.Value
+			}
+		}
+		if username == "" {
+			return
+		}
+		webSocketConnection, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		webSocketConnections[username] = webSocketConnection
+		go readWritePump("bob")
+	})
+
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
